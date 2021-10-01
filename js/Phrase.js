be7374e230fd116a9fcb0969825f682e2092b39b
Matthew Grant <amattwgrant@gmail.com>
2021-10-01 12:01:52 -0400

    Started project
    Created classes and constructors in js file

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,6 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+	constructor(phrase){
+		this.phrase = phrase;
+		this.hi = false;
+	}
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ const phrase = new Phrase ('Tell me where it hurts');
+
+ console.log(`Phrase - phrase: ${phrase.phrase}`);
