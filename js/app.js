bad44d9dddcbeb95e8b25e291a11b7c690695cd7
Matthew Grant <amattwgrant@gmail.com>
2021-10-03 16:57:39 -0400

    Working on event listener
    Working on event listener to capture letter that user guesses

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-// event listener to start game
-
+// variables
 let game;
 const startButton = document.querySelector('#btn__reset');
+const keyboard = document.querySelector('#qwerty');
 
+// event listener to start game
 startButton.addEventListener('click', () => {
 	game = new Game();
 	game.startGame();
 });
+
+// event listener to capture keystroke value
+keyboard.addEventListener('click', (e) => {
+	if (e.target && e.target.type === 'submit')
+	console.log(e.target.textContent);
+})
