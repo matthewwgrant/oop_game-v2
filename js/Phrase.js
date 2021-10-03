54ea1b484f2894890a8d68d68d9010c75f4747c9
Matthew Grant <amattwgrant@gmail.com>
2021-10-03 16:07:59 -0400

    phrase showing in console
    Phrases are now showing in console with proper classes attached

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,7 +7,19 @@ class Phrase {
 	 * Displays phrase on game board
 	 */
 	addPhraseToDisplay() {
-		
+		const ul = document.querySelector('#phrase ul');
+		const getPhrase = game.getRandomPhrase().phrase;
+		const letters = getPhrase.split('');
+		for ( let i = 0; i < letters.length; i++ ) {
+			const regex = /^[a-z]/;
+			if (  regex.test(letters[i]) ) {
+				const addLetter = `<li class="hide letter ${letters[i]}">${letters[i]}</li>`
+				ul.append(addLetter);
+			} else {
+				const addSpace = `<li class="space"> </li>`;
+				ul.append(addSpace);
+			}
+		}
 	}
 
 	// checkLetter() {
--- js/app.js
+++ js/app.js
@@ -2,19 +2,15 @@
  * Project 4 - OOP Game App
  * app.js */
 
- const logPhrase = (phrase) => {
- 	console.log(`Phrase - phrase: `, phrase.phrase);
- };
+ // const logPhrase = (phrase) => {
+ // 	console.log(`Phrase - phrase: `, phrase.phrase);
+ // };
 
 const game = new Game();
-
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
 // game.phrases.forEach((phrase, index) => {
 // 	console.log(`Phrase  ${index} - phrase: ${phrase.phrase}`);
 // });
 
-
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+phrase.addPhraseToDisplay();
